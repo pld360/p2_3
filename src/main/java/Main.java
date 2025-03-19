15cc29765393b13258abb76a52eacc2bb4b4a67c
pld360 <134562508+pld360@users.noreply.github.com>
2025-03-19 13:20:52 +0000

    gotowe zad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,17 @@
-
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.print("Enter first number: ");
-    int num1 = scanner.nextInt();
+    System.out.print("Wprowadź liczbę a: ");
+    int a = scanner.nextInt();
     
-    System.out.print("Enter second number: ");
-    int num2 = scanner.nextInt();
+    System.out.print("Wprowadź liczbę b: ");
+    int b = scanner.nextInt();
     
-    int sum = num1 + num2;
-    System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    int suma = a + b;
+    System.out.println("Suma " + a + " oraz " + b + " jest równa: " + suma);
     
     scanner.close();
   }
