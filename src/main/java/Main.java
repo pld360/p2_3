b7735b7764e6a94723ea77601cc9408916405904
pld360 <134562508+pld360@users.noreply.github.com>
2025-03-19 13:52:54 +0000

    gotowe nie akceptujace liter i nieujemnych

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,31 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    int a = 0, b = 0;
+
+    while (true) {
+      try {
+        System.out.print("Wprowadź liczbę a: ");
+        String input = scanner.nextLine();
+        a = Integer.parseInt(input);
+        if (a >= 0) break;
+        System.out.println("Liczba musi być nieujemna!");
+      } catch (NumberFormatException e) {
+        System.out.println("To nie jest liczba, wprowadź liczbę.");
+      }
+    }
     
-    System.out.print("Wprowadź liczbę a: ");
-    int a = scanner.nextInt();
-    
-    System.out.print("Wprowadź liczbę b: ");
-    int b = scanner.nextInt();
+     while (true) {
+      try {
+        System.out.print("Wprowadź liczbę b: ");
+        String input = scanner.nextLine();
+        b = Integer.parseInt(input);
+        if (b >= 0) break;
+        System.out.println("Liczba musi być nieujemna!");
+      } catch (NumberFormatException e) {
+        System.out.println("To nie jest liczba, wprowadź liczbę.");
+      }
+    }
     
     double suma = Math.sqrt(a) + Math.sqrt(b);
     System.out.println("Suma pierwiastków " + a + " oraz " + b + " jest równa: " + suma);
